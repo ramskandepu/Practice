5bd90c2687bbed6b2b59b74d3da1209040f58035
ram <ram@gmail.com>
2018-12-21 17:47:55 -0500

    maven

---
+++ src/main/java/org/maven/com/WelcomeRepository.java
@@ -0,0 +1,17 @@
+package org.maven.com;
+
+import org.eiichiro.gig.Repository;
+import org.eiichiro.jaguar.inject.Name;
+
+@Name("org.maven.com.WelcomeRepository")
+@Repository
+public class WelcomeRepository {
+
+	public Welcome get() {
+		// It's OK to get it from database.
+		Welcome welcome = new Welcome();
+		welcome.status = "READY";
+		return welcome;
+	}
+	
+}
